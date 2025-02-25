8877fb36c392f33d2adf8c3df9a92bd2f9847df6
Jiake404 <wujk25@mail2.sysu.edu.cn>
2025-02-25 14:39:07 +0800

    Delete GlacierLake_v2 folder

--- GlacierLake_v2/cal_lakealbedo.m
+++
@@ -1,7 +0,0 @@
-function [lake_albedo] = cal_lakealbedo(albedo_num,lake_depth)
-if albedo_num == 0
-    lake_albedo = (9702 + 1000.*exp(3.6*lake_depth))...
-            /(-539 + 20000.*exp(3.6*lake_depth)); 
-else
-lake_albedo = 0.1820+0.3512.*exp(-1.0774.*lake_depth);
-end
